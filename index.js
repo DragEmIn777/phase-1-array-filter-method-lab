4f1afbf249db0d5a39f32bbc39c1c48d6f4ad4d2
Matt Draghi <mtd5264@gmail.com>
2022-01-13 21:12:11 -0500

    commit tree c022ea81467d4a59ea6b2736bbf4b6340aaa6aae

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const findMatching = (driverNames, string) => {
+    return driverNames.filter((name) => {
+        return string.toUpperCase() === name.toUpperCase();
+    })
+}
+
+const fuzzyMatch = (driverNames, string) => {
+    return driverNames.filter((name) =>{
+        return string === name.substring(0, string.length);
+    })
+}
+
+const matchName = (drivers, string) => {
+    return drivers.filter(driver => driver.name === string)
+}
